b5e1b9abe4552580b7eb1b045f8d92a3366932b0
sodaranh <sodara.nhek@supinternet.fr>
2018-12-13 13:00:55 +0100

    second commit

--- app/src/main/java/com/example/sodara/android_things/MainActivity.kt
+++ app/src/main/java/com/example/sodara/android_things/MainActivity.kt
@@ -11,6 +11,13 @@ import java.io.IOException
 import java.util.*
 import com.google.android.things.contrib.driver.button.Button.OnButtonEventListener
 import com.google.android.things.contrib.driver.ht16k33.Ht16k33
+import com.google.android.things.pio.Pwm
+import com.google.android.things.contrib.driver.pwmservo.Servo
+import com.google.android.things.pio.PeripheralManager
+
+
+
+
 
 
 /**
@@ -37,25 +44,63 @@ class MainActivity : Activity() {
     private var mLedstrip: Apa102? = null
     private var LEDSTRIP_BRIGHTNESS = 1
     val buttonC = RainbowHat.openButtonC()
+    val buttonA = RainbowHat.openButtonA()
+    val buttonB = RainbowHat.openButtonB()
+    val segment = RainbowHat.openDisplay()
+    val servo = Servo("PWM0")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*val ledstrip = RainbowHat.openLedStrip()
+        ledstrip.brightness = 1
+        val rainbow = IntArray(RainbowHat.LEDSTRIP_LENGTH)
+        for (i in rainbow.indices) {
+            rainbow[i] = Color.HSVToColor(255, floatArrayOf(i * 360f / rainbow.size, 1.0f, 1.0f))
+        }
+        ledstrip.write(rainbow)
+
+        ledstrip.close()*/
+
+        segment.setBrightness(Ht16k33.HT16K33_BRIGHTNESS_MAX)
+        segment.display("HELLO WORLD")
+        segment.setEnabled(true)
+        segment.close()
         buttonC.setOnButtonEventListener(object : OnButtonEventListener {
             override fun onButtonEvent(button: Button, pressed: Boolean) {
                 finish()
-                val segment = RainbowHat.openDisplay()
-                segment.setBrightness(Ht16k33.HT16K33_BRIGHTNESS_MAX)
-                //segment.display("")
-                segment.setEnabled(true)
-                segment.close()
+            }
+        })
+        buttonB.setOnButtonEventListener(object : OnButtonEventListener {
+            override fun onButtonEvent(button: Button, pressed: Boolean) {
+                if (pressed) {
+                    servo.angle = servo.minimumAngle
+                    servo.setPulseDurationRange(1.0, 2.0)
+                    servo.setAngleRange(-90.0, 90.0)
+                    servo.setEnabled(true)
+                    Thread.sleep(100)
+                    servo.setEnabled(false)
+                }
+            }
+        })
+        buttonA.setOnButtonEventListener(object : OnButtonEventListener {
+            override fun onButtonEvent(button: Button, pressed: Boolean) {
+                if (pressed) {
+                    servo.angle = servo.maximumAngle
+                    servo.setPulseDurationRange(1.0, 2.0)
+                    servo.setAngleRange(-90.0, 90.0)
+                    servo.setEnabled(true)
+                    Thread.sleep(100)
+                    servo.setEnabled(false)
+                }
             }
         })
     }
     override fun onDestroy() {
         super.onDestroy()
-        butt
-
+        servo.setEnabled(false)
+        servo.close()
     }
 
 }
