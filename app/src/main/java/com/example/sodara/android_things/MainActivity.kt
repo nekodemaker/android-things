8d649b1ce2c93e5b3a7916b8046bc3ef4e3650ed
sodaranh <sodara.nhek@supinternet.fr>
2018-12-13 11:33:55 +0100

    button C to close the app

--- app/src/main/java/com/example/sodara/android_things/MainActivity.kt
+++ app/src/main/java/com/example/sodara/android_things/MainActivity.kt
@@ -1,7 +1,17 @@
 package com.example.sodara.android_things
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import com.google.android.things.contrib.driver.apa102.Apa102
+import com.google.android.things.contrib.driver.button.Button
+import com.google.android.things.contrib.driver.rainbowhat.RainbowHat
+import java.io.IOException
+import java.util.*
+import com.google.android.things.contrib.driver.button.Button.OnButtonEventListener
+import com.google.android.things.contrib.driver.ht16k33.Ht16k33
+
 
 /**
  * Skeleton of an Android Things activity.
@@ -24,9 +34,28 @@ import android.os.Bundle
  *
  */
 class MainActivity : Activity() {
-
+    private var mLedstrip: Apa102? = null
+    private var LEDSTRIP_BRIGHTNESS = 1
+    val buttonC = RainbowHat.openButtonC()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonC.setOnButtonEventListener(object : OnButtonEventListener {
+            override fun onButtonEvent(button: Button, pressed: Boolean) {
+                finish()
+                val segment = RainbowHat.openDisplay()
+                segment.setBrightness(Ht16k33.HT16K33_BRIGHTNESS_MAX)
+                //segment.display("")
+                segment.setEnabled(true)
+                segment.close()
+            }
+        })
     }
+    override fun onDestroy() {
+        super.onDestroy()
+        butt
+
+    }
+
 }
